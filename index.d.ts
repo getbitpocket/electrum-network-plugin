c8c8bf1b29910f3f6336f979414bbd6118f89bb9
Stefan Huber <stefan.huber@beyondit.at>
2016-04-08 10:06:54 +0200

    updated definition file

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,31 @@
-declare namespace electrum {
+interface Peer {
+        
+    new (host: string, port: number) : Peer;
     
-    export interface Peer {
-        
-        new (host: string, port: number) : Peer;
-        
-        connect() : Peer;        
-        disconnect() : Peer;        
-        sendRequest(request: {id?: any, method: string, params: Array<any>}) : Peer;
-        
-        on(event: string, eventHandler: ( (eventData?: any) => void ) ) : void; 
-        
-    }
+    connect() : Peer;        
+    disconnect() : Peer;        
+    sendRequest(request: {id?: any, method: string, params: Array<any>}) : Peer;
     
-    export interface NetworkDiscovery {
+    on(event: string, eventHandler: ( (eventData?: any) => void ) ) : void; 
         
-        init() : NetworkDiscovery;
-        getPeer(index?: number) : Peer;
-        sendRandomRequest(request: {id?: any, method: string, params: Array<any>}) : NetworkDiscovery;
-        
-        on(event: string, eventHandler: ( (eventData?: any) => void ) ) : void; 
-        
-    }
-    
 }
+    
+interface NetworkDiscovery {
+    
+    new () : NetworkDiscovery;
+        
+    init() : NetworkDiscovery;
+    getPeer(index?: number) : Peer;
+    sendRandomRequest(request: {id?: any, method: string, params: Array<any>}) : NetworkDiscovery;
+        
+    on(event: string, eventHandler: ( (eventData?: any) => void ) ) : void; 
+        
+}
+    
+declare var electrum: {
+    Peer: Peer;
+    NetworkDiscovery: NetworkDiscovery;
+};
 
 declare module "electrum" {
     export = electrum;
